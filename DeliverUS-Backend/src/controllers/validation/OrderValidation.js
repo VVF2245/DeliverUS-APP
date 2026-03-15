54e438f98fad780a341c45cf9796433e76257976
NTY8373 <julflocan@alum.us.es>
2026-03-15 13:37:06 +0100

    Correct quotation marks

--- DeliverUS-Backend/src/controllers/validation/OrderValidation.js
+++ DeliverUS-Backend/src/controllers/validation/OrderValidation.js
@@ -1,4 +1,4 @@
-import { Restaurant, Product, Order } from "#root/src/models/models.js"
+import { Restaurant, Product, Order } from '#root/src/models/models.js'
 import { check } from 'express-validator'
 
 // TODO: Include validation rules for create that should:
@@ -7,29 +7,29 @@ import { check } from 'express-validator'
 // 3. Check that products are available
 // 4. Check that all the products belong to the same restaurant
 const create = [
-    check('restaurantId').exists().withMessage('restaurantId is required').isInt().custom( async (value) => {
-        const restaurant = await Restaurant.findByPk(value)
-        if (!restaurant) throw new Error('Restaurant not found')
-    }),
-    check('products').exists().isArray({ min: 1 }).custom( async (products, {req}) => {
-        const productIds = products.map(p => p.productId)
-        const dbProducts = await Product.findAll({
-            where: { id: productIds}
-        })
+  check('restaurantId').exists().withMessage('restaurantId is required').isInt().custom(async (value) => {
+    const restaurant = await Restaurant.findByPk(value)
+    if (!restaurant) throw new Error('Restaurant not found')
+  }),
+  check('products').exists().isArray({ min: 1 }).custom(async (products, { req }) => {
+    const productIds = products.map(p => p.productId)
+    const dbProducts = await Product.findAll({
+      where: { id: productIds }
+    })
 
-        const restaurantIdSet = new Set(dbProducts.map(p => p.restaurantId))
-        if (restaurantIdSet.size > 1) throw new Error('All products must belong to the same restaurant')
-        
-        if (req.body.restaurantId && !restaurantIdSet.has(req.body.restaurantId)) {
-            throw new Error('Products do not match the restaurantId in the body')
-        }
-    }),
-    check('products.*.productId').exists().isInt().custom( async (value) => {
-        const product = await Product.findByPk(value)
-        if (!product) throw new Error('Product does not exist')
-        if (!product.availability) throw new Error('Product not available')
-    }),
-    check('products.*.quantity').exists().isInt({ min: 1 })
+    const restaurantIdSet = new Set(dbProducts.map(p => p.restaurantId))
+    if (restaurantIdSet.size > 1) throw new Error('All products must belong to the same restaurant')
+
+    if (req.body.restaurantId && !restaurantIdSet.has(req.body.restaurantId)) {
+      throw new Error('Products do not match the restaurantId in the body')
+    }
+  }),
+  check('products.*.productId').exists().isInt().custom(async (value) => {
+    const product = await Product.findByPk(value)
+    if (!product) throw new Error('Product does not exist')
+    if (!product.availability) throw new Error('Product not available')
+  }),
+  check('products.*.quantity').exists().isInt({ min: 1 })
 ]
 // TODO: Include validation rules for update that should:
 // 1. Check that restaurantId is NOT present in the body.
@@ -38,27 +38,26 @@ const create = [
 // 4. Check that all the products belong to the same restaurant of the originally saved order that is being edited.
 // 5. Check that the order is in the 'pending' state.
 const update = [
-    check('restaurantId').not().exists(),
-    check('products').exists().isArray({ min: 1 }).custom( async (products, { req }) => {
-        const order = await Order.findByPk(req.params.id)
-        const productIds = products.map(p => p.productId)
-        const dbProducts = await Product.findAll({
-            where: { id: productIds}
-        })
+  check('restaurantId').not().exists(),
+  check('products').exists().isArray({ min: 1 }).custom(async (products, { req }) => {
+    const order = await Order.findByPk(req.params.id)
+    const productIds = products.map(p => p.productId)
+    const dbProducts = await Product.findAll({
+      where: { id: productIds }
+    })
 
-        const restaurantIdSet = new Set(dbProducts.map(p => p.restaurantId))
-        if (restaurantIdSet.size > 1) throw new Error('All products must belong to the same restaurant')
+    const restaurantIdSet = new Set(dbProducts.map(p => p.restaurantId))
+    if (restaurantIdSet.size > 1) throw new Error('All products must belong to the same restaurant')
 
-        if ( !order ) throw new Error('Order not found')
-        if ( order.getStatus() !== 'pending' ) throw new Error('Order must be pending to edit')
-        
-    }),
-    check('products.*.productId').exists().isInt().custom( async (value) => {
-        const product = await Product.findByPk(value)
-        if (!product) throw new Error('Product does not exist')
-        if (!product.availability) throw new Error('Product not available')
-    }),
-    check('products.*.quantity').exists().isInt({ min: 1 }),
+    if (!order) throw new Error('Order not found')
+    if (order.getStatus() !== 'pending') throw new Error('Order must be pending to edit')
+  }),
+  check('products.*.productId').exists().isInt().custom(async (value) => {
+    const product = await Product.findByPk(value)
+    if (!product) throw new Error('Product does not exist')
+    if (!product.availability) throw new Error('Product not available')
+  }),
+  check('products.*.quantity').exists().isInt({ min: 1 })
 ]
 
 export { create, update }
