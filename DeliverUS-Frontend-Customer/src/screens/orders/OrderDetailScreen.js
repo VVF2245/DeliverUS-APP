b37c66e70da9de6ab1341125885bd184f2abb1a4
VVF2245 <luihercua@alum.us.es>
2026-05-06 12:52:25 +0200

    Está casi terminada la actualización de address pero todavia no se cambia la base de datos por bug

--- DeliverUS-Frontend-Customer/src/screens/orders/OrderDetailScreen.js
+++ DeliverUS-Frontend-Customer/src/screens/orders/OrderDetailScreen.js
@@ -1,11 +1,21 @@
 import { useEffect, useState } from 'react'
-import { FlatList, StyleSheet, View, ImageBackground } from 'react-native'
+import {
+  FlatList,
+  StyleSheet,
+  View,
+  ImageBackground,
+  Pressable,
+  Modal,
+  TextInput
+} from 'react-native'
 import TextRegular from '../../components/TextRegular'
 import TextSemiBold from '../../components/TextSemiBold'
 import ImageCard from '../../components/ImageCard'
 import restaurantBackground from '../../../assets/restaurantBackground.jpeg'
 import defaultProductImage from '../../../assets/product.jpeg'
 import { API_BASE_URL } from '@env'
+
+import { updateOrder } from '../../api/OrderEndpoints.js'
 
 export default function OrderDetailScreen({ navigation, route }) {
   const [order, setOrder] = useState({
@@ -14,6 +24,10 @@ export default function OrderDetailScreen({ navigation, route }) {
     shippingCosts: 0,
     restaurant: {}
   })
+  const [showAddressModal, setShowAddressModal] = useState(false)
+  const [newAddress, setNewAddress] = useState('')
+
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
 
   //MOCK
   useEffect(() => {
@@ -59,61 +73,95 @@ export default function OrderDetailScreen({ navigation, route }) {
     return date ? new Date(date).toLocaleString() : '-'
   }
 
+  const handleUpdateOrder = async values => {
+    try {
+      const updatedOrder = await updateOrder({
+        ...order,
+        address: newAddress
+      })
+
+      setOrder(prev => ({
+        ...prev,
+        ...updatedOrder
+      }))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const renderHeader = () => {
     return (
-      <ImageBackground
-        style={styles.ImageBackground}
-        source={
-          order.restaurant?.heroImage
-            ? {
-                uri: API_BASE_URL + '/' + order.restaurant.heroImage,
-                cache: 'force-cache'
-              }
-            : restaurantBackground
-        }
-      >
-        <View style={styles.headerContainer}>
-          {/* TÍTULO A LA IZQUIERDA */}
-          <TextSemiBold textStyle={styles.textTitle}>
-            Order #{order.id} - Total:{' '}
-            {(order.price + order.shippingCosts || 0).toFixed(2)} €
-          </TextSemiBold>
+      <>
+        <ImageBackground
+          style={styles.ImageBackground}
+          source={
+            order.restaurant?.heroImage
+              ? {
+                  uri: API_BASE_URL + '/' + order.restaurant.heroImage,
+                  cache: 'force-cache'
+                }
+              : restaurantBackground
+          }
+        >
+          <View style={styles.headerContainer}>
+            {/* TÍTULO A LA IZQUIERDA */}
+            <TextSemiBold textStyle={styles.textTitle}>
+              Order #{order.id} - Total:{' '}
+              {(order.price + order.shippingCosts || 0).toFixed(2)} €
+            </TextSemiBold>
 
-          <View style={styles.centerContent}>
-            <TextRegular textStyle={styles.text}>
-              Restaurant: {order.restaurant?.name}
-            </TextRegular>
-            <TextRegular textStyle={styles.text}>
-              Status: {order.status}
-            </TextRegular>
+            <View style={styles.centerContent}>
+              <TextRegular textStyle={styles.text}>
+                Restaurant: {order.restaurant?.name}
+              </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                Status: {order.status}
+              </TextRegular>
 
-            <TextRegular textStyle={styles.text}>
-              Created: {formatDate(order.createdAt)}
-            </TextRegular>
-            <TextRegular textStyle={styles.text}>
-              Started: {formatDate(order.startedAt)}
-            </TextRegular>
-            <TextRegular textStyle={styles.text}>
-              Sent: {formatDate(order.sentAt)}
-            </TextRegular>
-            <TextRegular textStyle={styles.text}>
-              Delivered: {formatDate(order.deliveredAt)}
-            </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                Created: {formatDate(order.createdAt)}
+              </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                Started: {formatDate(order.startedAt)}
+              </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                Sent: {formatDate(order.sentAt)}
+              </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                Delivered: {formatDate(order.deliveredAt)}
+              </TextRegular>
 
-            <TextRegular textStyle={styles.text}>
-              Address: {order.address}
-            </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                Address: {order.address}
+              </TextRegular>
 
-            <TextRegular textStyle={styles.text}>
-              Price: {order.price.toFixed(2)} €
-            </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                Price: {order.price.toFixed(2)} €
+              </TextRegular>
 
-            <TextRegular textStyle={styles.text}>
-              ShippingCosts: {order.shippingCosts.toFixed(2)} €
-            </TextRegular>
+              <TextRegular textStyle={styles.text}>
+                ShippingCosts: {order.shippingCosts.toFixed(2)} €
+              </TextRegular>
+            </View>
           </View>
-        </View>
-      </ImageBackground>
+        </ImageBackground>
+
+        {order.status === 'pending' && (
+          <View style={styles.buttonContainer}>
+            <Pressable
+              style={styles.button}
+              onPress={() => {
+                setNewAddress(order.address)
+                setShowAddressModal(true)
+              }}
+            >
+              <TextRegular textStyle={styles.buttonText}>
+                Editar dirección
+              </TextRegular>
+            </Pressable>
+          </View>
+        )}
+      </>
     )
   }
 
@@ -162,6 +210,82 @@ export default function OrderDetailScreen({ navigation, route }) {
         keyExtractor={item => item.id.toString()}
         contentContainerStyle={{ paddingBottom: 10 }}
       />
+      <Modal visible={showAddressModal} transparent={true} animationType="fade">
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <TextSemiBold textStyle={styles.modalTitle}>
+              Edit Address
+            </TextSemiBold>
+
+            <TextInput
+              style={styles.input}
+              value={newAddress}
+              onChangeText={setNewAddress}
+              placeholder="Introduce the new address"
+            />
+
+            <View style={styles.modalButtons}>
+              {/*CANCELAR*/}
+              <Pressable
+                style={[styles.modalButton, styles.cancelButton]}
+                onPress={() => setShowAddressModal(false)}
+              >
+                <TextRegular textStyle={styles.buttonText}>Cancel</TextRegular>
+              </Pressable>
+
+              {/*ACEPTAR*/}
+              <Pressable
+                style={[styles.modalButton, styles.confirmButton]}
+                onPress={() => {
+                  setShowConfirmModal(true)
+                }}
+              >
+                <TextRegular textStyle={styles.buttonText}>Accept</TextRegular>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal visible={showConfirmModal} transparent={true} animationType="fade">
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <TextSemiBold textStyle={styles.modalTitle}>
+              Confirm changes?
+            </TextSemiBold>
+
+            <TextRegular textStyle={{ textAlign: 'center', marginBottom: 20 }}>
+              Are you sure you want to change address?
+            </TextRegular>
+
+            <View style={styles.modalButtons}>
+              {/*NO*/}
+              <Pressable
+                style={[styles.modalButton, styles.cancelButton]}
+                onPress={() => setShowConfirmModal(false)}
+              >
+                <TextRegular textStyle={styles.buttonText}>No</TextRegular>
+              </Pressable>
+
+              {/*SÍ*/}
+              <Pressable
+                style={[styles.modalButton, styles.confirmButton]}
+                onPress={async () => {
+                  try {
+                    await handleUpdateOrder()
+                    setShowAddressModal(false)
+                    setShowConfirmModal(false)
+                  } catch (error) {
+                    console.log(error)
+                  }
+                }}
+              >
+                <TextRegular textStyle={styles.buttonText}>Yes</TextRegular>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
@@ -202,5 +326,61 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 10
+  },
+  buttonContainer: {
+    padding: 15,
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#2ecc71',
+    padding: 12,
+    borderRadius: 8,
+    width: '80%',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalContent: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20
+  },
+  modalTitle: {
+    fontSize: 18,
+    marginBottom: 15,
+    textAlign: 'center'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  modalButton: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginHorizontal: 5
+  },
+  cancelButton: {
+    backgroundColor: '#e74c3c'
+  },
+  confirmButton: {
+    backgroundColor: '#2ecc71'
   }
 })
